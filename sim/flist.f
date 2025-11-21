f052a2c65feb34088bc4c6e0ff411c92318d6679
shuwentao <418674879@qq.com>
2025-11-21 17:00:53 +0800

    feature::add labs02-e

--- sim/flist.f
+++ sim/flist.f
@@ -2,12 +2,12 @@
 ../uvm-1.1a/src/uvm.sv
 ../uvm-1.1a/src/dpi/uvm_dpi.cc
 
-+incdir+../labs/lab02-d
-../labs/lab02-d/tb.sv
-../labs/lab02-d/my_transaction.sv
-../labs/lab02-d/my_driver.sv
-../labs/lab02-d/my_monitor.sv
-../labs/lab02-d/my_sequence.sv
-../labs/lab02-d/master_agent.sv
-../labs/lab02-d/my_env.sv
-../labs/lab02-d/my_test.sv
++incdir+../labs/lab02-e
+../labs/lab02-e/tb.sv
+../labs/lab02-e/my_transaction.sv
+../labs/lab02-e/my_driver.sv
+../labs/lab02-e/my_monitor.sv
+../labs/lab02-e/my_sequence.sv
+../labs/lab02-e/master_agent.sv
+../labs/lab02-e/my_env.sv
+../labs/lab02-e/my_test.sv
