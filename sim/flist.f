4e7e39b53b17200291d0e9f57ae58b23bc310eec
shuwentao <418674879@qq.com>
2025-11-22 18:43:47 +0800

    feature:add lab03-a

--- sim/flist.f
+++ sim/flist.f
@@ -2,14 +2,15 @@
 ../uvm-1.1a/src/uvm.sv
 ../uvm-1.1a/src/dpi/uvm_dpi.cc
 
-+incdir+../labs/lab03
-../labs/lab03/tb.sv
-../labs/lab03/my_transaction.sv
-../labs/lab03/my_transaction_da3.sv
-../labs/lab03/my_driver.sv
-../labs/lab03/my_monitor.sv
-../labs/lab03/my_sequence.sv
-../labs/lab03/master_agent.sv
-../labs/lab03/my_env.sv
-../labs/lab03/my_test.sv
-../labs/lab03/my_test_type_da3.sv
++incdir+../labs/lab03-a
+../labs/lab03-a/tb.sv
+../labs/lab03-a/my_transaction.sv
+../labs/lab03-a/my_transaction_da3.sv
+../labs/lab03-a/my_driver.sv
+../labs/lab03-a/my_monitor.sv
+../labs/lab03-a/my_sequence.sv
+../labs/lab03-a/master_agent.sv
+../labs/lab03-a/my_env.sv
+../labs/lab03-a/my_test.sv
+../labs/lab03-a/my_test_type_da3.sv
+../labs/lab03-a/my_test_inst_da3.sv
